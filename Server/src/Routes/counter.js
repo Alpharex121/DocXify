e82ddd5e12acb2ccb06380d3ec26c3d8453bf2bf
Alpharex121 <arpitkoshta08@gmail.com>
2024-08-19 13:46:56 +0530

    configuring counter

--- Server/src/Routes/counter.js
+++ Server/src/Routes/counter.js
@@ -1,40 +1,65 @@
+require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const router = express.Router();
+const vercelEdge = require("@vercel/edge-config");
 
-const filePath = path.join("/tmp/", "counter.json");
+const edgeConfig = vercelEdge.createClient(process.env.ANOTHER_EDGE_CONFIG);
+console.log(process.env.ANOTHER_EDGE_CONFIG);
 
-const readCounter = () => {
-  try {
-    const data = fs.readFileSync(filePath, "utf8");
-    const json = JSON.parse(data);
-    return json.count;
-  } catch (error) {
-    console.error("Error reading counter file:", error);
-    return 0;
-  }
-};
+// const filePath = path.join("/tmp/", "counter.json");
 
-const writeCounter = (count) => {
-  try {
-    const json = { count };
-    fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
-  } catch (error) {
-    console.error("Error writing counter file:", error);
-  }
-};
+// const readCounter = () => {
+//   try {
+//     const data = fs.readFileSync(filePath, "utf8");
+//     const json = JSON.parse(data);
+//     return json.count;
+//   } catch (error) {
+//     console.error("Error reading counter file:", error);
+//     return 0;
+//   }
+// };
 
-router.get("/", (req, res) => {
-  const count = readCounter();
+// const writeCounter = (count) => {
+//   try {
+//     const json = { count };
+//     fs.writeFileSync(filePath, JSON.stringify(json, null, 2));
+//   } catch (error) {
+//     console.error("Error writing counter file:", error);
+//   }
+// };
+
+router.get("/", async (req, res) => {
+  const count = await edgeConfig.get("counter");
   res.json({ count });
 });
 
-router.post("/increment", (req, res) => {
-  let count = readCounter();
-  count += 1;
-  writeCounter(count);
-  res.json({ count });
+router.post("/increment", async (req, res) => {
+  try {
+    let count = await edgeConfig.get("counter");
+    const updateEdgeConfig = await fetch(
+      `https://api.vercel.com/v1/edge-config/${process.env.CLIENT_ID}/`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          operation: "update",
+          key: "counter",
+          value: count++,
+        }),
+      }
+    );
+    const result = await updateEdgeConfig.json();
+    console.log(result);
+    count++;
+    res.json({ count });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
